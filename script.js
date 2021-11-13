69475058fb4ef9d9b946095beae5c9170507ed42
sayed muhsin <sdmuhsin@gmail.com>
2021-11-13 10:10:37 +0530

    working version, placeholder data

---
+++ script.js
@@ -0,0 +1,72 @@
+
+
+
+$(document).ready(function(){
+    /* Text style */
+    $("#textStyle").textbanner({cycles:100,growth:150});
+    $("#nextButton").attr("disabled",true)
+
+    function addVideoAndPlay(link){
+
+        $("#intContainer").append(
+        `<video class = "mx-auto" id = "studentVid" controls="controls" height="480" width="640">
+            <source id = "vidSrc" src = "${link}"  type="video/mp4">
+        </video>`);
+        $('#studentVid').trigger('play');
+        $('#studentVid').prop('muted','false')
+
+        
+    }
+    function loadTestAndExecute(test, stageId){
+        $(stageId).empty()
+        $(stageId).append("<div id = 'intContainer' class = 'container-fluid d-flex flex-column ' style= 'min-height:75%'></div>")
+        $("#intContainer").append(`<h4>${test.title}</h4>`)
+        let interval = 1000;
+        test.asserts.forEach((assert,i)=>{
+            setTimeout(function(){
+                $("#intContainer").append(`<div id = "assert-item-${i}"class = "px-4 mx-4 d-flex justify-content-start align-items-start"> <p>${assert}</p> <div class = "mt-1 spinner-${i} mx-4" id = 'loading' ></div></div>`)
+            },interval)
+            interval += 1000;  
+        })
+        
+    }
+
+    function passAssertItems(test,interval=1000){
+        let n = test.asserts.length;
+        for(let i = 0; i < n; i++){
+            setTimeout(()=>{
+                $(`#assert-item-${i}`).css(`color`,'#90EE90');
+                $(`.spinner-${i}`).remove();
+                $(`#assert-item-${i}`).append(`<p class = "mx-4 "> &check; </p>`);
+                if(i==n-1){
+                    $("#nextButton").attr("disabled",false);
+                    if(test.videoLink){
+                        addVideoAndPlay(test.videoLink)
+                    }
+                }
+            },interval);
+            interval += 1000;
+        }
+        
+    }
+    
+    let testCounter = 0;
+    function nextTest(){
+        if(testCounter < tests.length){
+            loadTestAndExecute(tests[testCounter],"#stage");
+            passAssertItems( tests[testCounter], tests[testCounter].asserts.length * 1000)
+            testCounter += 1;
+        }
+    }
+    
+    $("#nextButton").click(function(){
+        $("#nextButton").attr("disabled",true)
+        nextTest();
+    });
+
+    $(".startButton").click(function(){
+        $("#sadBg")[0].play();
+        $("#sadbg").prop("muted",false);
+        nextTest();
+    });
+})
---
+++ tests.js
@@ -0,0 +1,31 @@
+let tests = [
+        {
+            "title":"Test : Gain Vishal's Approval",
+            "asserts":[
+                "Assert that Brad is a great teacher",
+                "Assert that Brad tells cool jokes ",
+                "Assert that Brad taught me SpringBoot",
+                "Assert that Brad will be missed"
+            ],
+            "videoLink":"https://dl.dropboxusercontent.com/s/q0knpz1yjw75mlx/sadbg.mp4?dl=0"
+        },
+        {
+            "title":"Test : Gain Shikha's approval",
+            "asserts":[
+                "Assert that Brad answeras all my questions",
+                "Assert that Brad made me an excellent programmer ",
+                "Assert that Brad taught me to save money",
+                "Assert that Brad will be missed"
+            ],
+            "videoLink":"https://dl.dropboxusercontent.com/s/q0knpz1yjw75mlx/sadbg.mp4?dl=0"
+        },
+        {
+            "title":"Test : Gain Yogita's approval",
+            "asserts":[
+                "Should be A",
+                "Should be B",
+                "Should be C",
+                "Should be D"
+            ]
+        }
+    ]
