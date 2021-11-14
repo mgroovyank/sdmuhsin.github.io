1a4870bc1e87ebdc55e57a3044caa7c1fe57d54b
Shikha Gupta <42310453+shikhagupta9@users.noreply.github.com>
2021-11-14 20:55:50 +0530

    Shikha's changes

--- tests.js
+++ tests.js
@@ -12,9 +12,10 @@ let tests = [
         {
             "title":"Test : Gain Shikha's approval",
             "asserts":[
-                "Assert that Brad answeras all my questions",
+                "Assert that Brad has really interesting and engaging classes",   
+                "Assert that Brad encourages my questions",
                 "Assert that Brad made me an excellent programmer ",
-                "Assert that Brad taught me to save money",
+                "Assert that Brad suggests amazing Books and websites",
                 "Assert that Brad will be missed"
             ],
             "videoLink":"https://dl.dropboxusercontent.com/s/q0knpz1yjw75mlx/sadbg.mp4?dl=0"
@@ -35,6 +36,14 @@ let tests = [
                 "Assert that be Brad shares his knowledge with students",
                 "Assert that Brad helps to increase my skill set",
                 "Assert that Brad always wins"
+            ],
+            "videoLink":"https://dl.dropboxusercontent.com/s/q0knpz1yjw75mlx/sadbg.mp4?dl=0"
+        },
+        {
+            "title":"Test : Gain Everyone's Approval",
+            "asserts":[
+                "Assert that Brad forms a dream team!",
+                "Assert that Brad turns us all into millionaires!"
             ],
             "videoLink":"https://dl.dropboxusercontent.com/s/q0knpz1yjw75mlx/sadbg.mp4?dl=0"
         }
